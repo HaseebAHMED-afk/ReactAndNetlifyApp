2890757209f3123bc48349d7cc59b790f9187502
HaseebAHMED-afk <haseeb.ahmed456123@gmail.com>
2021-02-17 16:12:16 +0500

    Deployed App

--- src/App.js
+++ src/App.js
@@ -5,18 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src='https://www.netlify.com/img/press/logos/logomark.png' className="App-logo" alt="logo" />
+        </div>
+        <h1>Netlify and React</h1>
       </header>
     </div>
   );
